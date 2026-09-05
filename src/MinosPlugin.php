ad6d39a958c4e173332f4b16f9b7c3f558f04890
Anton Shabouta <zloyusr@gmail.com>
2026-09-05 15:16:16 +0300

    [atlas+20]: Simplify inline returns

--- src/MinosPlugin.php
+++ src/MinosPlugin.php
@@ -18,6 +18,7 @@ class MinosPlugin implements Plugin
 
     public static function get(): static
     {
+        // @mago-expect lint:inline-variable-return
         /** @var static $plugin */
         $plugin = filament(app(static::class)->getId());
 
